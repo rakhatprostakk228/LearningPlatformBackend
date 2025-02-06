cf003e7952f4d617e460ca242de7a31600016ae8
Rakhimbekov Rakhat <rkhantemir@mail.ru>
2025-02-06 23:41:38 +0500

    Backend commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://learningplatformfrontend.onrender.com",
     credentials: true
 }));
 
