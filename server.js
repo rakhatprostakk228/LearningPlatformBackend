8893ffab622f71f234cf045bd01e5af1878635ae
Rakhimbekov Rakhat <rkhantemir@mail.ru>
2025-02-10 21:52:53 +0500

    backend

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 
-// Request Logging
+// Request log
 app.use((req, res, next) => {
     console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
     next();
